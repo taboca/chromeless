96dca4393cd278dd9118a1b8727e08e830f551c3
Marcio Galli <marciogalli@taboca.local>
2010-10-08 13:38:08 -0700

    Jetpack means to create new window. The example opens the internal resource URL which is the first_browsr example from Lloyd. The window reference has a .tabs property which is not the internal developer-browser tabs, yet. But the exercise is a good start towards a thinking where there is an object like Browser which represents the whole Window, Developer page with an internal browsers, and more.

--- extension/chromeless/lib/main.js
+++ extension/chromeless/lib/main.js
@@ -32,11 +32,40 @@ exports.main = function(options, callbacks) {
     contentScript: script,
     contentScriptWhen: "ready",
     onMessage: function(message) {
-      console.log(message);
+      //console.log(message);
     }
   });
 
   pageWorkers.add(page);
+
+
+  /* New Windows - also following some notes from Lloyd that the app 
+     can simply launch in a new window.
+
+     This Window implementation is interesting because it represents, 
+     in a way, a vision towards a browser environments. It represents
+     a Window which internal properties such as Tabs, Title. This could
+     well serve as a reference point to what we are tring to do. A 
+     window is the main entry point associated with one developer-browser
+     internal page and it indeed may have tabs and other things. But 
+     the tabs will eventually be an abstraction to the various 
+     browser elements the HTML page has. 
+     
+     Ref: Window = http://127.0.0.1:8888/#module/core/windows
+
+  */
+
+
+  var windows = require("windows").browserWindows;
+  //windows.openWindow("resource://-chromeless-data/first_browser/index.html");
+  windows.openWindow({
+    url: "resource://-chromeless-data/first_browser/index.html",
+    onOpen: function(window) {
+      // do stuff like listen for content
+      // loading.
+	console.log(window.body.innerHTML);
+    }
+  });
 
 }
 
