53ac12e8317d4b6db8300a714a72f345157ca3ab
Marcio Galli <taboca@gmail.com>
2010-11-08 20:24:47 -0200

    Including bits of pieces of Firefox code, to have the XUL tabbrowser implementation available. This implementation uses DTDs defined in the en-US, and also some preferences.

--- template/app-extension-chrome/defaults/preferences/prefs.js
+++ template/app-extension-chrome/defaults/preferences/prefs.js
@@ -7,3 +7,13 @@ pref("javascript.options.strict", true);
 // *entrypoint* xul
 pref("toolkit.defaultChromeURI", "chrome://chromeless/content/main.xul");
 
+pref("browser.tabs.closeWindowWithLastTab",true);
+pref("browser.chrome.site_icons",true);
+pref("browser.chrome.favicons",true);
+pref("browser.tabs.loadInBackground",true);
+pref("browser.tabs.animate",true);
+pref("browser.tabs.insertRelatedAfterCurrent",true);
+pref("accessibility.typeaheadfind",true);
+pref("browser.tabs.closeButtons",true);
+pref("browser.tabs.autoHide",true);
+pref("browser.tabs.closeWindowWithLastTab",true);
